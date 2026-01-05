734d973356bb286970da5fb776c80a3c3c71edfd
Rafael Andrade <arkikis181@gmail.com>
2026-01-05 12:19:58 -0300

    first commit

---
+++ src/test/java/com/uninter/vidaplusapi/VidaplusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninter.vidaplusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaplusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
